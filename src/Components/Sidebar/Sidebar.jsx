95ce74e1f378ec0f1ff4834bd9709356b2de5a05
borgesmj <borgesmj19@gmail.com>
2024-05-07 11:05:34 -0500

    adding navlinks to the navbar icons

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,18 +1,35 @@
-import { User,UsersThree, HeartStraight, GearSix, EnvelopeSimple } from "@phosphor-icons/react";
-import "./Sidebar.css"
+import {
+  User,
+  UsersThree,
+  HeartStraight,
+  GearSix,
+  EnvelopeSimple,
+} from "@phosphor-icons/react";
+import "./Sidebar.css";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className='w-fit h-dvh sidebar-bg flex flex-col justify-evenly px-2'>
+    <div className="w-fit h-dvh sidebar-bg flex flex-col justify-evenly px-2">
       <section className="flex flex-col justify-between h-40">
-        <EnvelopeSimple size={32} weight="regular" color="white" />  
-        <HeartStraight size={32} weight="regular" color="white"  />
-        <UsersThree size={32} weight="regular" color="white"  />
-        <User size={32} weight="regular" color="white"  />
+        <NavLink to="/messages">
+          <EnvelopeSimple size={32} weight="regular" color="white" /> <span className="hidden">Messages</span>
+        </NavLink>
+        <NavLink to="/favorites">
+          <HeartStraight size={32} weight="regular" color="white" />
+        </NavLink>
+        <NavLink to="groups">
+          <UsersThree size={32} weight="regular" color="white" />
+        </NavLink>
+        <NavLink to="profile">
+          <User size={32} weight="regular" color="white" />
+        </NavLink>
       </section>
-        <GearSix size={32} weight="regular" color="white"  />
+      <NavLink to="/settings">
+        <GearSix size={32} weight="regular" color="white" />
+      </NavLink>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
