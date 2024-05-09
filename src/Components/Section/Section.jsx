87e9d2ea00789866b52a0c43f8a10a259285cbef
borgesmj <borgesmj19@gmail.com>
2024-05-09 08:58:28 -0500

    section font color according to theme

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Section = ({ children, darkTheme }) => {
   return (
-    <section className={`${!darkTheme ? 'bg-[#f5f5f5]' : 'bg-[#333f58]'} h-dvh .options-bar py-8 ml-12 options-bar`}>
+    <section className={`${!darkTheme ? 'bg-[#f5f5f5] text-[#333333]' : 'bg-[#333f58] text-[#e0e0e0]'} h-dvh .options-bar py-8 ml-12 options-bar`}>
       {children}
     </section>
   );
