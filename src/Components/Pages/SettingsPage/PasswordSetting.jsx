5246d3e32f401fc646362b3fabe12d5c35d1188b
borgesmj <borgesmj19@gmail.com>
2024-05-09 10:19:30 -0500

    adding passwords form

--- src/Components/Pages/SettingsPage/PasswordSetting.jsx
+++ src/Components/Pages/SettingsPage/PasswordSetting.jsx
@@ -1,12 +1,64 @@
-import Section from '../../Section/Section'
-import React from 'react'
+import Section from "../../Section/Section";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { ArrowLeft } from "@phosphor-icons/react";
 
-const PasswordSetting = ({darkTheme}) => {
+const PasswordSetting = ({ darkTheme }) => {
+
+  const evalRegex = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+  }
+
+  const evalRepeat = () => {
+    console.log('repeat')
+  }
   return (
-      <Section darkTheme={darkTheme}>
-        password
-      </Section>
-  )
-}
+    <Section darkTheme={darkTheme}>
+      <div className="flex flex-col w-full h-dvh">
+        <NavLink
+          to="/settings"
+          className={`w-[50px] h-[50px] ml-[20px] mt-[20px] flex justify-center items-center rounded-full border-solid border-[1px] ${
+            !darkTheme ? "border-[#917800]" : "border-[#dbffff]"
+          }`}
+        >
+          <ArrowLeft size={32} />
+        </NavLink>
+        <form className={`w-4/5 flex flex-col mt-5 items-center justify-center p-4 mx-auto ${!darkTheme ? 'bg-[#479ae0]' : 'bg-[#0A192F]'} rounded-lg`}>
+          <div className="w-full flex flex-col my-2 py-2">
+            <label htmlFor="current-password" className="w-full text-center mb-2">Contraseña Actual:</label>
+            <input
+              type="password"
+              name="current-password"
+              id="current-password"
+              className={`${!darkTheme ? 'text-[#333333]' : 'text-[#1E88E5]'} px-2`}
+            />
+          </div>
+          <div className="w-full flex flex-col my-2 py-2">
+            <label htmlFor="new-password" className="w-full text-center mb-2">Nueva Contraseña:</label>
+            <input
+              type="password"
+              name=""
+              id="new-password"
+              className={`${!darkTheme ? 'text-[#333333]' : 'text-[#1E88E5]'} px-2`}
+              onChange={(e) => evalRegex(e)}
+            />
+          </div>
+          <div className="w-full flex flex-col my-2 py-2">
+            <label htmlFor="repeat-new-password" className="w-full text-center mb-2">
+              Repita la nueva contraseña:
+            </label>
+            <input
+              type="password"
+              name="new-password"
+              id="repeat-new-password"
+              className={`${!darkTheme ? 'text-[#333333]' : 'text-[#1E88E5]'} px-2`}
+            />
+          </div>
+        </form>
+      </div>
+    </Section>
+  );
+};
 
-export default PasswordSetting
+export default PasswordSetting;
--- src/Components/Pages/SettingsPage/ProfileSettings.jsx
+++ src/Components/Pages/SettingsPage/ProfileSettings.jsx
@@ -1,11 +1,22 @@
-import Section from "../../Section/Section"
+import Section from "../../Section/Section";
+import { NavLink } from "react-router-dom";
+import { ArrowLeft } from "@phosphor-icons/react";
 
-const ProfileSettings = ({darkTheme}) => {
+const ProfileSettings = ({ darkTheme }) => {
   return (
-      <Section darkTheme={darkTheme}>
-        profile
-      </Section>
-  )
-}
+    <Section darkTheme={darkTheme}>
+      <div className="flex flex-col w-full h-dvh">
+        <NavLink
+          to="/settings"
+          className={`w-[50px] h-[50px] ml-[20px] mt-[20px] flex justify-center items-center rounded-full border-solid border-[1px] ${
+            !darkTheme ? "border-[#917800]" : "border-[#dbffff]"
+          }`}
+        >
+          <ArrowLeft size={32} />
+        </NavLink>
+      </div>
+    </Section>
+  );
+};
 
-export default ProfileSettings
+export default ProfileSettings;
