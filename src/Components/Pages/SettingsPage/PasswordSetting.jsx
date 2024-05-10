9eeb41f233b61fca7c640196b88719b7b5bba78e
borgesmj <borgesmj19@gmail.com>
2024-05-10 09:45:22 -0500

    improve new passqword form

--- src/Components/Pages/SettingsPage/PasswordSetting.jsx
+++ src/Components/Pages/SettingsPage/PasswordSetting.jsx
@@ -1,18 +1,22 @@
 import Section from "../../Section/Section";
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { ArrowLeft } from "@phosphor-icons/react";
+import { ArrowLeft, Lock, Eye,EyeSlash  } from "@phosphor-icons/react";
 
 const PasswordSetting = ({ darkTheme }) => {
+  const [revealedPassword, setRevealedPassword] = useState(false)
 
-  const evalRegex = (e) => {
-    e.preventDefault()
-    console.log(e.target.value)
+  const revealPassword = () => {
+      setRevealedPassword(!revealedPassword)
   }
+  const evalRegex = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
 
   const evalRepeat = () => {
-    console.log('repeat')
-  }
+    console.log("repeat");
+  };
   return (
     <Section darkTheme={darkTheme}>
       <div className="flex flex-col w-full h-dvh">
@@ -24,37 +28,111 @@ const PasswordSetting = ({ darkTheme }) => {
         >
           <ArrowLeft size={32} />
         </NavLink>
-        <form className={`w-4/5 flex flex-col mt-5 items-center justify-center p-4 mx-auto ${!darkTheme ? 'bg-[#479ae0]' : 'bg-[#0A192F]'} rounded-lg`}>
-          <div className="w-full flex flex-col my-2 py-2">
-            <label htmlFor="current-password" className="w-full text-center mb-2">Contraseña Actual:</label>
-            <input
-              type="password"
-              name="current-password"
-              id="current-password"
-              className={`${!darkTheme ? 'text-[#333333]' : 'text-[#1E88E5]'} px-2`}
-            />
+        <form
+          className={`w-4/5 flex flex-col mt-5 items-center justify-center p-4 mx-auto ${
+            !darkTheme ? "bg-[#dbffff]" : "bg-[#0A192F]"
+          } rounded-lg`}
+        >
+          <h3 className="text-center">Configura una nueva contraseña</h3>
+          <div
+            className={`p-2 m-2 border-solid border -[1px] ${
+              !darkTheme ? "border-[#917800]" : "border-[#dbffff]"
+            } rounded-[10px]`}
+          >
+            <Lock size={32} />
           </div>
+          <span className="text-xs text-center">
+            Debe ser, al menos, una contraseña de 8 caracteres. Debe incluir
+            caracteres especiales */+?
+          </span>
           <div className="w-full flex flex-col my-2 py-2">
-            <label htmlFor="new-password" className="w-full text-center mb-2">Nueva Contraseña:</label>
-            <input
-              type="password"
-              name=""
-              id="new-password"
-              className={`${!darkTheme ? 'text-[#333333]' : 'text-[#1E88E5]'} px-2`}
-              onChange={(e) => evalRegex(e)}
-            />
-          </div>
-          <div className="w-full flex flex-col my-2 py-2">
-            <label htmlFor="repeat-new-password" className="w-full text-center mb-2">
-              Repita la nueva contraseña:
+            <label
+              htmlFor="current-password"
+              className="w-full text-left mb-2 ml-2"
+            >
+              Contraseña Actual:
             </label>
-            <input
-              type="password"
-              name="new-password"
-              id="repeat-new-password"
-              className={`${!darkTheme ? 'text-[#333333]' : 'text-[#1E88E5]'} px-2`}
-            />
+            <p
+              className={`w-full border-solid border-[1px] rounded-[10px] ${
+                !darkTheme ? "border-[#917800]" : "border-[#dbffff]"
+              } flex flex-row justify-around items-center hover:cursor-pointer  relative h-10`}
+            >
+              <input
+                type={revealedPassword ? "text" : "password"}
+                name="current-password"
+                id="current-password"
+                className={`bg-transparent p-2 outline-none ${
+                  !darkTheme
+                    ? "placeholder:text-[#5c5c5c]"
+                    : "placeholder:text-[#e0e0e0]"
+                } hover:cursor-pointer`}
+                placeholder="Introduzca contraseña"
+              />
+              <span onClick={revealPassword} className="absolute right-0 top-0 bottom-0 h-full flex items-center">
+              {!revealedPassword ? <Eye size={28} /> : <EyeSlash size={28} />}
+              </span>
+            </p>
           </div>
+          <div className="w-full flex flex-col my-2 py-2">
+            <label
+              htmlFor="new-password"
+              className="w-full text-left mb-2 ml-2"
+            >
+              Nueva contraseña:
+            </label>
+            <p
+              className={`w-full border-solid border-[1px] rounded-[10px] ${
+                !darkTheme ? "border-[#917800]" : "border-[#dbffff]"
+              } flex flex-row justify-around items-center hover:cursor-pointer  relative h-10`}
+            >
+              <input
+                type={revealedPassword ? "text" : "password"}
+                name="new-password"
+                id="new-password"
+                className={`bg-transparent p-2 outline-none ${
+                  !darkTheme
+                    ? "placeholder:text-[#5c5c5c]"
+                    : "placeholder:text-[#e0e0e0]"
+                } hover:cursor-pointer`}
+                placeholder="Introduzca contraseña"
+              />
+              <span onClick={revealPassword} className="absolute right-0 top-0 bottom-0 h-full flex items-center">
+              {!revealedPassword ? <Eye size={28} /> : <EyeSlash size={28} />}
+              </span>
+            </p>
+          </div>
+          <div className="w-full flex flex-col my-2 py-2">
+            <label
+              htmlFor="confirm-new-password"
+              className="w-full text-left mb-2 ml-2"
+            >
+              Confirma nueva contraseña:
+            </label>
+            <p
+              className={`w-full   border-solid border-[1px] rounded-[10px] ${
+                !darkTheme ? "border-[#917800]" : "border-[#dbffff]"
+              } flex flex-row justify-around items-center hover:cursor-pointer relative h-10`}
+            >
+              <input
+                type={revealedPassword ? "text" : "password"}
+                name="confirm-new-password"
+                id="confirm-new-password"
+                className={`bg-transparent p-2 outline-none ${
+                  !darkTheme
+                    ? "placeholder:text-[#5c5c5c]"
+                    : "placeholder:text-[#e0e0e0]"
+                } hover:cursor-pointer absolute left-0 top-0 bottom-0 h-full`}
+                placeholder="Introduzca contraseña"
+              />
+              <span onClick={revealPassword} className="absolute right-0 top-0 bottom-0 h-full flex items-center">
+              {!revealedPassword ? <Eye size={28} /> : <EyeSlash size={28} />}
+              </span>
+            </p>
+          </div>
+          <div className="w-full flex flex-col my-2 py-2">
+            <input type="submit" value="Cambiar contraseña" className={`relative flex justify-center items-center rouned-[5px] ${!darkTheme ? 'bg-[#479ae0]' : 'bg-[#3ce0bc]'} overflow-hidden cursor-pointer border-none h-[50px] rounded-[10px]`} />
+          </div>
+          <NavLink to="/settings" className="underline">Cancelar</NavLink>
         </form>
       </div>
     </Section>
