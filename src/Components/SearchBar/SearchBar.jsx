ab63e1d0bd7cac0bfefe7f71e565d1898349a814
borgesmj <borgesmj19@gmail.com>
2024-05-19 15:20:30 -0500

    Update SearchBar component styles and layout

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -2,7 +2,7 @@ import {MagnifyingGlass} from '@phosphor-icons/react'
 
 const SearchBAr = () => {
   return (
-    <form className='search-bar bg-red-100 w-full flex justify-center items-center h-12 my-6 px-2'>
+    <form className='search-bar w-full flex justify-center items-center h-12 my-6 px-2'>
       <p className='w-full px-2 py-2  flex flex-row justify-between items-center bg-white border-solid border-[1px] border-black rounded-[5rem]'>
         <input type="text" name="" id="" placeholder='Buscar...' className=' focus:w-full outline-none' />
         <span  className='border-solid transition-[opacity] opacity-100 rounded-full border-1px border-black max-h-full' ><MagnifyingGlass size={20}/></span>
