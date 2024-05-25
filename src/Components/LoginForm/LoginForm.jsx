ecdfcf1e7bebe0dbbedaf70d19f4138f7aaef337
borgesmj <borgesmj19@gmail.com>
2024-05-25 05:28:46 -0500

    Refactor LoginForm component: removed unused imports, simplified code, and improved error handling

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -3,7 +3,7 @@ import FormField from "../FormField/FormField";
 import SubmitBtn from "../SubmitBtn/SubmitBtn";
 import { At, Lock } from "@phosphor-icons/react";
 import { auth, db } from "../../Process/Firebase";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 // Toastyfy
 import { ToastContainer, toast, Bounce } from "react-toastify";
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 // firebase
 import { collection, getDocs, query, where } from "firebase/firestore";
 // React router dom
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = ({
   id,
@@ -21,12 +21,10 @@ const LoginForm = ({
   setLoading,
   setCurrentUser,
   openToastSuccess,
-  currentUser
 }) => {
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPW, setLoginPW] = useState("");
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = async () => {
     const lowercaseEmail = loginEmail.toLowerCase();
@@ -37,51 +35,41 @@ const LoginForm = ({
         lowercaseEmail,
         loginPW
       );
-      setLoading(false);
-      // setLoginEmail("");
-      // setLoginPW("");
       const fetchUserDataByUsername = async () => {
-        try {
-          const usersRef = collection(db, "users");
-          const q = query(usersRef, where("email", "==", lowercaseEmail));
-          const querySnapShot = await getDocs(q);
-
-          const filteredUser = [];
-          querySnapShot.forEach((doc) => {
-            filteredUser.push(doc.data());
-          });
-          if (filteredUser.length > 0) {
-            setCurrentUser(filteredUser[0])
-          } else {
-            openToastError("Usuario no existe");
-            return null;
-          }
-        } catch (error) {
-          console.log("error trayendo usuario", error);
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, where("email", "==", lowercaseEmail));
+        const querySnapShot = await getDocs(q);
+        const filteredUser = [];
+        querySnapShot.forEach((doc) => {
+          filteredUser.push(doc.data());
+        });
+        if (filteredUser.length > 0) {
+          return filteredUser[0];
+        } else {
+          openToastError("Usuario no existe");
+          return null;
         }
       };
-      fetchUserDataByUsername()
-      if (currentUser){
-        console.log("login sucess");
-        openToastSuccess('Validación Exitosa')
-        navigate('/chats')
+
+      const userData = await fetchUserDataByUsername();
+      if (userData) {
+        setCurrentUser(userData);
+        openToastSuccess('Validación Exitosa');
+        navigate('/chats');
+      } else {
+        setLoading(false);
       }
     } catch (error) {
       console.log(error);
+      setLoading(false);
       if (error.code === "auth/invalid-credential") {
-        setLoading(false);
         openToastError("Email y/o contraseña incorrecto");
-        return;
-      } else if (error.code === "mail is not defined") {
-        setLoading(false);
+      } else if (error.code === "auth/user-not-found") {
         openToastError("Usuario no existe");
-        return;
       } else if (error.code === "auth/user-disabled") {
-        setLoading(false);
         openToastError("Usuario Inhabilitado");
-      } else if (error.code) {
-        setLoading(false);
-        openToastError("Ups!! Algo salio mal");
+      } else {
+        openToastError("Ups!! Algo salió mal");
       }
     }
   };
