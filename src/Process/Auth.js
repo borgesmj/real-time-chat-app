96ecd71cc24c5f511bb9909981f93d2f4ebe9d9c
borgesmj19 <borgesmj19@gmail.com>
2024-05-31 14:28:02 +0000

    moving logind process to auth file, setting the UID to the local storage

--- src/Process/Auth.js
+++ src/Process/Auth.js
@@ -1,3 +1,15 @@
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "./Firebase"
+
+export const logIn = (email, password) => {
+    const results = signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+    )
+    return results
+}
+
 export const LogOut = () => {
     window.localStorage.removeItem("userUID")
 }
