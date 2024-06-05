011f9de564a18672be0a96020b5b237be6838239
borgesmj19 <borgesmj19@gmail.com>
2024-06-05 14:18:37 +0000

    formating code

--- src/Components/ChatsList/ChatsList.jsx
+++ src/Components/ChatsList/ChatsList.jsx
@@ -1,15 +1,18 @@
-import ChatCard from "../ChatCard/ChatCard"
-import SearchBAr from "../SearchBar/SearchBar"
+import ChatCard from "../ChatCard/ChatCard";
+import SearchBAr from "../SearchBar/SearchBar";
 
-const ChatsList = ({darkTheme, openChat}) => {
-    return (
-        <div className="w-full">
-            <SearchBAr/>
-            <ul id="chatslist" className="w-full overflow-y-auto absolute overflow-x-hidden">
-            <ChatCard darkTheme={darkTheme} openChat={openChat}/>
-        </ul>
-        </div>
-      )
-    }
+const ChatsList = ({ darkTheme, openChat }) => {
+  return (
+    <div className="w-full">
+      <SearchBAr />
+      <ul
+        id="chatslist"
+        className="w-full overflow-y-auto absolute overflow-x-hidden"
+      >
+        <ChatCard darkTheme={darkTheme} openChat={openChat} />
+      </ul>
+    </div>
+  );
+};
 
-export default ChatsList
+export default ChatsList;
