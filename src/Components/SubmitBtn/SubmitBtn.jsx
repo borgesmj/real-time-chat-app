a4610ff6648f423238ab44dea97434d9d3ec92fe
borgesmj19 <borgesmj19@gmail.com>
2024-06-08 12:44:23 +0000

    changin text color

--- src/Components/SubmitBtn/SubmitBtn.jsx
+++ src/Components/SubmitBtn/SubmitBtn.jsx
@@ -12,7 +12,7 @@ const SubmitBtn = ({ btnText, handleSubmit, btnIsActive }) => {
       className={`w-[120px] h-[40px] p-3 flex justify-center items-center rounded-md relative transition-all active:scale-100 active:shadow-[0px_0px_0px_0px_var(--text-200)] ${
         !btnIsActive
           ? "cursor-not-allowed bg-gray-500"
-          : "bg-gradient-to-r from-[var(--accent-100)] to-[var(--accent-200)] border-solid border-[var(--primary-300)] border-[1px] text-[var(--text-100)] scale-105 shadow-[0px_0px_4px_2px_var(--text-200)]"
+          : "bg-gradient-to-r from-[var(--accent-100)] to-[var(--accent-200)] border-solid border-[var(--primary-300)] border-[1px] text-[#fefefe] scale-105 shadow-[0px_0px_4px_2px_var(--text-200)]"
       }`}
       onClick={handleButton}
     >
