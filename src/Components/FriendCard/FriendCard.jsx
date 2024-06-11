b378b26c73aaed54c03a812fc17025458936ee14
borgesmj19 <borgesmj19@gmail.com>
2024-06-11 21:16:11 +0000

    changed: bg colors, hover bg colors

--- src/Components/FriendCard/FriendCard.jsx
+++ src/Components/FriendCard/FriendCard.jsx
@@ -1,5 +1,10 @@
 import { Check } from "@phosphor-icons/react";
-import {acceptFriendRequest, rejectRequest, deleteFriend, removeRequest } from '../../Process/handleFriendsList'
+import {
+  acceptFriendRequest,
+  rejectRequest,
+  deleteFriend,
+  removeRequest,
+} from "../../Process/handleFriendsList";
 
 const FriendCard = ({
   friends,
@@ -10,38 +15,41 @@ const FriendCard = ({
   profilePic,
   currentUserDocId,
   newFriendID,
-  openModal
+  openModal,
 }) => {
-
   const handleClick = (e) => {
-    e.preventDefault()
-    openModal(username)
-  }
-
+    e.preventDefault();
+    openModal(username);
+  };
 
   return (
-    <div className="bg-white shadow-2xl p-4 h-fit w-[300px] rounded-2xl my-2">
+    <div className="bg-[var(--bg-100)] shadow-2xl p-4 h-fit w-[300px] rounded-2xl my-2 flex flex-col items-center mt-6">
+      <img src={profilePic} alt="" className="w-16 rounded-full -mt-8" />
       <div className="card-header flex flex-row justify-start p-2">
-        <img
-          src={profilePic}
-          alt=""
-          className="h-12 w-auto rounded-full"
-        />
         <div className="ml-2">
-          <h2 className="font-semibold">{name}</h2>
           <a href={`/user/${username}`}>
-            <span>{`@${username}`}</span>
+            <span className="text-xl">{`@${username}`}</span>
           </a>
         </div>
       </div>
-      <div className="options-buttons flex flex-row justify-around">
+      <div className="options-buttons flex flex-row justify-between w-full">
         {/* Amigo agregado*/}
         {friends && (
           <>
-            <button className="w-1/2 flex flex-row items-center" onClick={(e)=> {handleClick(e)}}>
+            <button
+              className="w-[45%] flex flex-row bg-blue-400 justify-center items-center rounded-2xl border-solid border-2 border-transparent hover:bg-white hover:text-blue-400 hover:border-blue-400 transition-all duration-200 ease-linear"
+              onClick={(e) => {
+                handleClick(e);
+              }}
+            >
               Enviar mensaje
             </button>
-            <button className="w-1/2 p-4 bg-red-500 rounded-2xl" onClick={() => {deleteFriend(currentUserDocId, newFriendID)}}>
+            <button
+              className="w-[45%] p-2 bg-red-500 rounded-2xl border-solid border-2 border-transparent hover:bg-white hover:text-red-500 hover:border-red-500 transition-all duration-200 ease-linear"
+              onClick={() => {
+                deleteFriend(currentUserDocId, newFriendID);
+              }}
+            >
               Eliminar
             </button>
           </>
@@ -50,7 +58,12 @@ const FriendCard = ({
          */}
         {requestSent && (
           <>
-            <button className="w-full p-4 bg-blue-500 rounded-2xl" onClick={() => {removeRequest(currentUserDocId, newFriendID)}}>
+            <button
+              className="w-full p-4 bg-blue-500 border-solid border-2 border-transparent rounded-2xl transition-all  hover:bg-white hover:border-blue-500 hover:text-blue-500"
+              onClick={() => {
+                removeRequest(currentUserDocId, newFriendID);
+              }}
+            >
               Cancelar Solicitud
             </button>
           </>
@@ -58,10 +71,20 @@ const FriendCard = ({
         {/*Aceptar/rechazar Solicitud  de amistad recibida*/}
         {requestReceived && (
           <>
-            <button className="w-1/2 p-4 bg-green-500 rounded-2xl" onClick= {() => {acceptFriendRequest(currentUserDocId, newFriendID)}}>
+            <button
+              className="w-1/2 border-solid border-transparent border-2 p-4 bg-green-500 rounded-2xl hover:bg-white hover:text-green-500 hover:border-green-500"
+              onClick={() => {
+                acceptFriendRequest(currentUserDocId, newFriendID);
+              }}
+            >
               Aceptar
             </button>
-            <button className="w-1/2 p-4 bg-red-500 rounded-2xl" onClick={() => {rejectRequest(currentUserDocId, newFriendID)}}>
+            <button
+              className="w-1/2 border-solid border-transparent border-2 p-4 bg-red-500 rounded-2xl hover:bg-white hover:text-red-500 hover:border-red-500 "
+              onClick={() => {
+                rejectRequest(currentUserDocId, newFriendID);
+              }}
+            >
               Rechazar
             </button>
           </>
